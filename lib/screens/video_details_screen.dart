76d13eceebd23fdf03cb7d9c2ab25af1096dfdee
Sumia Iqbal <iqbalsumia30@gmail.>
2025-09-16 03:00:03 -0700

    added video details screen

---
+++ lib/screens/video_details_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class VideoDetailsScreen extends StatelessWidget {
+const VideoDetailsScreen({Key? key}) : super(key: key);
+
+@override
+Widget build(BuildContext context) {
+return Container();
+}
+}
